7c96324b993311593e479916a38032205eaf903a
tangwai <tmichael11314@gmail.com>
2023-10-17 02:58:36 -0500

    add entry points add module CSS

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    menu: './src/module/menuTab.js',
+    home: './src/module/homeTab.js',
+    contact: './src/module/contactTab.js',            
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
