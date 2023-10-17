ccd9507fd11ef790c4856ea489922e12a9df92b5
tangwai <tmichael11314@gmail.com>
2023-10-17 05:03:06 -0500

    add individual buttons append buttons to buttonsContainer

--- src/util.js
+++ src/util.js
@@ -1,8 +1,29 @@
-//so tab creation won't duplicate
-export function createTabButton(tabName) {
-    const tabButton = document.createElement('div');
-    tabButton.id = tabName;
-    tabButton.classList.add('tab');
-    tabButton.textContent = tabName;
-    return tabButton;
-}
+// //so tab creation won't duplicate
+// export function createTabButton(tabName) {
+//     const tabButton = document.createElement('div');
+//     tabButton.id = tabName;
+//     tabButton.classList.add('tab');
+//     tabButton.textContent = tabName;
+//     return tabButton;
+// }
+export function createTabs() {
+    const homeTabButton = document.createElement('button');
+    homeTabButton.classList.add('tab');
+    homeTabButton.id = "Home";
+    homeTabButton.textContent = "Home";
+    
+    const contactTabButton = document.createElement('button');
+    contactTabButton.classList.add('tab');
+    contactTabButton.id = "Contact";
+    contactTabButton.textContent = "Contact";
+    
+    const menuTabButton = document.createElement('button');
+    menuTabButton.classList.add('tab');
+    menuTabButton.id = "Menu";
+    menuTabButton.textContent = "Menu";
+    
+    const buttonContainer = document.getElementById('buttonContainer');
+    buttonContainer.appendChild(homeTabButton);
+    buttonContainer.appendChild(contactTabButton);
+    buttonContainer.appendChild(menuTabButton);
+};
