0a6e3cb7893d22e5be70a7c355c8b116645b0ab2
tangwai <tmichael11314@gmail.com>
2023-10-18 09:19:44 -0500

    test > have all tab buttons in header

--- src/module/homeTab.js
+++ src/module/homeTab.js
@@ -1,14 +1,24 @@
-import '../util.js';
+// import * as util from '../util.js';
 import './homeTab.css';
+
+// util.createHeader();
+// util.createButtonsContainer();
+// util.createTabs();
 
 export function createHomeTab() {
     const homeContent = document.createElement('div');
+    homeContent.classList.add("homeContent")
     homeContent.textContent = 'Contact home content';
+
     const homeButton = document.getElementById('Home');
-    homeButton.addEventListener('click', () => {
-      displayTabContent(createHomeTab);
-    });
-    
     homeContent.appendChild(homeButton);
+    
+  
+    // const contactButton = document.getElementById('Contact');
+    // contactContent.appendChild(contactButton);
+    
+    // const menuButton = document.getElementById('Menu');
+    // menuContent.appendChild(menuButton);
+
     return homeContent;
 };
